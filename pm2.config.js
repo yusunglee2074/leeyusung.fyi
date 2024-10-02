70a1b5608dfd7693b1a788d079c55332d4602918
yusunglee2074 <lys0333@gmail.com>
2024-10-02 11:33:07 +0900

    update next version and add pm2.config.js file

---
+++ pm2.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: "leeyusung.fyi",
+      script: "npm",
+      args: "start",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
